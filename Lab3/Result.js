ee5f47ba9db3bf6b8751d66e51af7b6231e8592a
huytqDE200146 <huytqde200146@gmail.com>
2026-06-07 00:53:44 +0700

    update file quan trong

--- Lab3/Result.js
+++ Lab3/Result.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 
 class Result extends Component {
   render() {
-    const { score, total, onRestart } = this.props;
+    const { score, total } = this.props;
 
     return (
       <div>
         <h2>Quiz Ended</h2>
         <p>Your Score: {score} / {total}</p>
-        <button onClick={onRestart}>Play Again</button>
       </div>
     );
   }
